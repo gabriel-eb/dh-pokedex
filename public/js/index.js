3755c43cb5eacfce53011ecf18a9a651e5891cc8
gabriel-eb <gabo_espburg@hotmail.com>
2021-09-06 20:44:11 -0500

    Obtener imagen del pokemon

--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,27 @@ window.addEventListener('load', () => {
   fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
     .then(res => res.json())
     .then(res => {
-      res.results.map(pokemon => populatePokelist(pokemon))
+      res.results.map(pokemon => {
+        populatePokelist(pokemon)
+        const list = document.querySelectorAll('li')
+        for (let choosedPokemon of list) {
+          fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon.name)
+            .then(detail => detail.json())
+            .then(detail => {
+              console.log(detail)
+              choosedPokemon.addEventListener('click', event => {
+                const divPokemon = document.querySelector('.pokedetail');
+                divPokemon.innerHTML = `
+            <img src="${detail.sprites.front_default}"/>
+            <h2>${pokemon.name}</h2>
+          `
+              })
+            })
+          }
+      })
     })
+
+  
 })
 
 function populatePokelist(pokemon) {
